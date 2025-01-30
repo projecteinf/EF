0c66a5b9f5d6133fa0ca474bb127a0d50ad3a8a7
projecteinf <projecteinf@boscdelacoma.cat>
2025-01-30 08:02:34 -0700

    Null values

---
+++ App Examples/Console App/04.NullValues/Program.cs
@@ -0,0 +1,11 @@
+﻿int notNullValue = 5;
+int? nullableValue = null; // Utilize ? to make a value type nullable
+
+// notNullValue = null; // error CS0037: Cannot convert null to 'int' because it is a non-nullable value type
+
+Console.WriteLine($"notNullValue: {notNullValue}");
+Console.WriteLine($"nullableValue:{nullableValue}");
+Console.WriteLine($"nullableValue default value: {nullableValue.GetValueOrDefault()}");
+
+nullableValue = 10;
+Console.WriteLine($"nullableValue:{nullableValue}");

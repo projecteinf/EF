5044000f2e43cbd833057a9ea639efc36bde8b49
projecteinf <96139692+projecteinf@users.noreply.github.com>
2025-01-30 18:18:25 +0100

    Null values and operator ?, ??

--- App Examples/Console App/04.NullValues/Program.cs
+++ App Examples/Console App/04.NullValues/Program.cs
@@ -9,3 +9,51 @@ Console.WriteLine($"nullableValue default value: {nullableValue.GetValueOrDefaul
 
 nullableValue = 10;
 Console.WriteLine($"nullableValue:{nullableValue}");
+
+// Codi utilitzant "?" 
+// Retorna la suma d'una llista de números
+double SumNumbers(List<double[]> setsOfNumbers, int indexOfSetToSum)
+{
+    return setsOfNumbers?[indexOfSetToSum]?.Sum() ?? double.NaN;
+}
+
+double SumNumbersClassic(List<double[]> setsOfNumbers, int indexOfSetToSum)
+{
+    {
+    if (setsOfNumbers == null || indexOfSetToSum < 0 || indexOfSetToSum >= setsOfNumbers.Count)
+    {
+        return double.NaN;
+    }
+
+    double[] selectedSet = setsOfNumbers[indexOfSetToSum];
+    if (selectedSet == null)
+    {
+        return double.NaN;
+    }
+
+    double sum = 0;
+    foreach (double number in selectedSet)
+    {
+        sum += number;
+    }
+
+    return sum;
+}
+
+Console.WriteLine(SumNumbers(null, 0));  // output: NaN
+Console.WriteLine(SumNumbersClassic(null, 0));
+  
+List<double[]?> numbers =
+[
+    [1.0, 2.0, 3.0],
+    null
+];
+
+
+Console.WriteLine(SumNumbers(numbers, 0));  // output: 6
+Console.WriteLine(SumNumbersClassic(numbers, 0));
+ 
+Console.WriteLine(SumNumbers(numbers, 1)); // output: NaN
+Console.WriteLine(SumNumbersClassic(numbers, 1));
+
+  

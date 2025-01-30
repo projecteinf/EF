3519bfa1552ddf93eb06b92754fec94d5d6be313
projecteinf <96139692+projecteinf@users.noreply.github.com>
2025-01-30 15:14:17 +0100

    Ordenació de valors segons capacitat de representació

--- App Examples/Console App/02.SizeLimits/Program.cs
+++ App Examples/Console App/02.SizeLimits/Program.cs
@@ -1,22 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine($"sbyte \n\tMinValue: {sbyte.MinValue.ToString("N")}, \n\tMaxValue: {sbyte.MaxValue.ToString("N")}");
+Console.WriteLine($"byte \n\tMinValue: {byte.MinValue.ToString("N")}, \n\tMaxValue: {byte.MaxValue.ToString("N")}");
+
+Console.WriteLine($"short \n\tMinValue: {short.MinValue.ToString("N")}, \n\tMaxValue: {short.MaxValue.ToString("N")}");
+Console.WriteLine($"ushort \n\tMinValue: {ushort.MinValue.ToString("N")}, \n\tMaxValue: {ushort.MaxValue.ToString("N")}");
+
 Console.WriteLine($"int \n\tMinValue: {int.MinValue.ToString("N")}, \n\tMaxValue: {int.MaxValue.ToString("N")}");
 Console.WriteLine($"uint \n\tMinValue: {uint.MinValue.ToString("N")}, \n\tMaxValue: {uint.MaxValue.ToString("N")}");
 
 Console.WriteLine($"long \n\tMinValue: {long.MinValue.ToString("N")}, \n\tMaxValue: {long.MaxValue.ToString("N")}");
 Console.WriteLine($"ulong \n\tMinValue: {ulong.MinValue.ToString("N")}, \n\tMaxValue: {ulong.MaxValue.ToString("N")}");
 
-Console.WriteLine($"byte \n\tMinValue: {byte.MinValue.ToString("N")}, \n\tMaxValue: {byte.MaxValue.ToString("N")}");
-
-Console.WriteLine($"sbyte \n\tMinValue: {sbyte.MinValue.ToString("N")}, \n\tMaxValue: {sbyte.MaxValue.ToString("N")}");
-
-Console.WriteLine($"short \n\tMinValue: {short.MinValue.ToString("N")}, \n\tMaxValue: {short.MaxValue.ToString("N")}");
-Console.WriteLine($"ushort \n\tMinValue: {ushort.MinValue.ToString("N")}, \n\tMaxValue: {ushort.MaxValue.ToString("N")}");
-
-
-
 Console.WriteLine($"float \n\tMinValue: {float.MinValue.ToString("N")}, \n\tMaxValue: {float.MaxValue.ToString("N")}");
-
-Console.WriteLine($"double \n\tMinValue: {double.MinValue.ToString("N")}, \n\tMaxValue: {double.MaxValue.ToString("N")}");
 
 Console.WriteLine($"decimal \n\tMinValue: {decimal.MinValue.ToString("N")}, \n\tMaxValue: {decimal.MaxValue.ToString("N")}");
 
+Console.WriteLine($"double \n\tMinValue: {double.MinValue.ToString("N")}, \n\tMaxValue: {double.MaxValue.ToString("N")}");

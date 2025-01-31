439b0622335a8ece5bbfc088d6738532e99642aa
projecteinf <projecteinf@boscdelacoma.cat>
2025-01-31 08:26:03 -0700

    Casting numeric values - Arrodoniment segons valor parell/imparell

--- App Examples/Console App/06.casting/Program.cs
+++ App Examples/Console App/06.casting/Program.cs
@@ -21,8 +21,22 @@ namespace casting
             WriteLine($"Convert cast ToString().: int: {Convert.ToString(myInt)}");
             WriteLine($"Convert cast ToDouble().: int: {Convert.ToDouble(myInt)}");
             WriteLine($"Convert cast ToInt32().: double: {Convert.ToInt32(myDouble)}");
-            WriteLine($"Convert cast ToInt32().: double: {Convert.ToInt32(9.49)}");
-            WriteLine($"Convert cast ToInt32().: double: {Convert.ToInt32(9.50)}");
+
+
+            double[] numExamples = new[]
+            { 9.49, 9.50, 9.51, 10.49, 10.50, 10.51 };
+            foreach (double numExample in numExamples)
+            {
+                if (numExample == 9.5) {
+                    WriteLine($"{(int)numExample} és imparell s'arrodoneix el valor");
+                }
+                else {
+                    if (numExample == 10.5) {
+                        WriteLine($"{(int)numExample} és parell trunca el valor");
+                    }
+                }
+                WriteLine($"{numExample} ToInt32: {Convert.ToInt32(numExample)}");
+            }
         }
     }
 }

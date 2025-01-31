0503728d3c49cf984bb9bd4e655f31da5b7a7503
projecteinf <projecteinf@boscdelacoma.cat>
2025-01-31 08:13:47 -0700

    Casting numeric values

---
+++ App Examples/Console App/06.casting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit casting
+            int myInt = 9;
+            double myDouble = myInt;
+            WriteLine($"Implicit casting: int: {myInt} , double: {myDouble}");
+
+            myDouble = 9.78;
+            // Explicit casting
+            myInt = (int)myDouble;  // (int) is a cast operator
+            WriteLine($"Explicit casting (int): int: {myInt} , double: {myDouble}");
+
+            // Convert class
+            WriteLine($"Convert cast ToString().: int: {Convert.ToString(myInt)}");
+            WriteLine($"Convert cast ToDouble().: int: {Convert.ToDouble(myInt)}");
+            WriteLine($"Convert cast ToInt32().: double: {Convert.ToInt32(myDouble)}");
+            WriteLine($"Convert cast ToInt32().: double: {Convert.ToInt32(9.49)}");
+            WriteLine($"Convert cast ToInt32().: double: {Convert.ToInt32(9.50)}");
+        }
+    }
+}

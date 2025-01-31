c474d7a4c7d89cf4acd25585a9008c93722912f9
projecteinf <projecteinf@boscdelacoma.cat>
2025-01-31 11:04:19 -0700

    Utilitzant TryParse per a la conversió de dades

---
+++ App Examples/Console App/07.TryParser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+// dotnet run 100 5,3
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: dotnet run <argument1> <argument2>");
+            return;
+        }
+
+
+        int price;
+        if (int.TryParse(args[0], out price))
+        {
+            WriteLine($"The price is {price}€.");
+        }
+        else
+        {
+            WriteLine($"Conversion failed for: {args[0]}");
+        }
+
+        if (int.TryParse(args[1], out price))
+        {
+            WriteLine($"There are {price} products.");
+        }
+        else
+        {
+            WriteLine($"Conversion failed for: {args[1]}");
+        }
+    }
+}
+
---
+++ Documentació CSharp/Referències.md
@@ -0,0 +1,3 @@
+# Referències web
+## Standard Date and Time Format Strings
+https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings

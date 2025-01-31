664750ed7e974c3db3a012f4e84d36d33a3efe36
projecteinf <projecteinf@boscdelacoma.cat>
2025-01-31 07:24:27 -0700

    Exemple foreach i descripció del seu funcionament

---
+++ App Examples/Console App/05.forEach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console; // Good practice
+
+string[] names = { "John", "Doe", "Jane", "Doe" };
+
+foreach (string name in names)
+{
+    WriteLine(name);
+}
+
+/* 
+Per utilitzar un foreach, la classe ha de tenir un mètode GetEnumerator() que retorna un objecte que implementa la interfície IEnumerator. 
+Aquesta interfície conté els mètodes MoveNext() i Current. MoveNext() avança l'iterador a l'element següent i Current retorna l'element actual.
+*/
+
+IEnumerator enames = names.GetEnumerator();
+while (enames.MoveNext())
+{
+    string name = (string)enames.Current; // Current is read-only!
+    WriteLine($"{name} has {name.Length} characters.");
+}

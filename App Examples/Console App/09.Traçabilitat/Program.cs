98c433978aae91553b8fd550595b229c0f6c1492
projecteinf <96139692+projecteinf@users.noreply.github.com>
2025-02-03 14:46:50 +0100

    Aplicació exemple - Registre d'errors i traçabilitat

--- App Examples/Console App/09.Traçabilitat/Program.cs
+++ App Examples/Console App/09.Traçabilitat/Program.cs
@@ -1,6 +1,19 @@
 ﻿using System.Diagnostics;
 using System.IO;
 
+
+/*
+
+Punts importants a recordar:
+
+    Listeners compartits: Un cop afegits a Trace.Listeners, els listeners s'utilitzen globalment a tota l'aplicació.
+    Tots els listeners reben els missatges: Quan fas Trace.WriteLine(), tots els listeners existents escriuen el missatge.
+    Trace.AutoFlush: Assegura que els missatges s'escriguin immediatament al fitxer (sense necessitat d'un Flush() manual).
+    Listeners per defecte: Si no afegeixes cap listener, per defecte, .NET només escriu els logs a la sortida de depuració.
+
+*/
+
+
 namespace Instrumenting
 {
     class Program
@@ -19,5 +32,4 @@ namespace Instrumenting
 Execució: 
    dotnet run --configuration Release # Només s'escriuen els missatges de Trace
    dotnet run --configuration Debug # S'escriuen els missatges de Debug i Trace
-   
 */

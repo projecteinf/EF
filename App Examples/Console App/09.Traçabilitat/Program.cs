67d1c2abb4afe607bb176aeeb512d885d6fb9d1f
projecteinf <96139692+projecteinf@users.noreply.github.com>
2025-02-03 14:51:23 +0100

    Documentació sobre - TRACE SWITCH

--- App Examples/Console App/09.Traçabilitat/Program.cs
+++ App Examples/Console App/09.Traçabilitat/Program.cs
@@ -3,13 +3,25 @@ using System.IO;
 
 
 /*
-
 Punts importants a recordar:
 
     Listeners compartits: Un cop afegits a Trace.Listeners, els listeners s'utilitzen globalment a tota l'aplicació.
     Tots els listeners reben els missatges: Quan fas Trace.WriteLine(), tots els listeners existents escriuen el missatge.
     Trace.AutoFlush: Assegura que els missatges s'escriguin immediatament al fitxer (sense necessitat d'un Flush() manual).
     Listeners per defecte: Si no afegeixes cap listener, per defecte, .NET només escriu els logs a la sortida de depuració.
+
+Execució: 
+   dotnet run --configuration Release # Només s'escriuen els missatges de Trace
+   dotnet run --configuration Debug # S'escriuen els missatges de Debug i Trace
+
+TRACE SWITCH
+
+
+0    Off          This will output nothing
+1    Error        This will output only errors
+2    Warning      This will output errors and warnings
+3    Info         This will output errors, warnings, and information
+4    Verbose      This will output all levels
 
 */
 
@@ -28,8 +40,3 @@ namespace Instrumenting
     }
 }
 
-/* 
-Execució: 
-   dotnet run --configuration Release # Només s'escriuen els missatges de Trace
-   dotnet run --configuration Debug # S'escriuen els missatges de Debug i Trace
-*/

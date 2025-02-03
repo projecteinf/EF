a9b90ede0f35385f723a326d3280e3071d6b1c56
projecteinf <projecteinf@boscdelacoma.cat>
2025-02-03 10:52:55 -0700

    Projecte exemple amb una llibreria i programa per a tests unitaris

---
+++ App Examples/ClassLib/01.Calculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace BoscComa.Calculadora
+{
+    public class Calculadora
+    {
+        public decimal Sumar(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+    }
+}
+
---
+++ App Examples/UnitTests/01.CalculatorLibUnitTests/CalculatorLibUnitTest.cs
@@ -0,0 +1,30 @@
+﻿namespace BoscComa.Calculadora;
+
+/*
+    Per executar els tests:
+
+    dotnet test
+*/
+public class UnitTestCalculadora
+{
+/* 
+    Arrange: This part will declare and instantiate variables for input and output.
+    •Act: This part will execute the unit that you are testing. In our case, that
+            means calling the method that we want to test.
+    •Assert: This part will make one or more assertions about the output. An
+            assertion is a belief that if not true indicates a failed test. For example, when
+            adding 2 and 2 we would expect the result would be 4. */
+    [Fact]
+    public void Sumar()
+    {
+        decimal a = 2;
+        decimal b = 2;
+        decimal expected = 4;
+        var calc = new Calculadora();
+
+        // act
+        decimal actual = calc.Sumar(a, b);
+        // assert
+        Assert.Equal(expected, actual);
+    }
+}

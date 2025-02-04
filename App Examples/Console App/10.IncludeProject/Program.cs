33bb44441cb89e51320ba0e3a838449c4b6dfe29
projecteinf <projecteinf@boscdelacoma.cat>
2025-02-04 11:12:43 -0700

    Programa exemple per incloure una llibreria (amb tipus definits) en el programa principal

---
+++ App Examples/Console App/10.IncludeProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+using Calc = BoscComa.Calculadora.Calculadora;  // Using alias directive. El nom de l'espai de noms i el de la classe coincideixen i el compilador no sap diferenciar l'un de l'altre.
+
+namespace BoscComa.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoscComa.Calculadora.Calculadora calc = new Calc();
+            WriteLine(calc.Sumar(1, 2));
+        }
+    }
+}
+

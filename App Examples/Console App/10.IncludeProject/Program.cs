c69d2ebafe869a10c9ff132ea9449ce8cb556adc
projecteinf <projecteinf@boscdelacoma.cat>
2025-02-04 11:14:15 -0700

    Programa exemple per incloure una llibreria (amb tipus definits) en el programa principal

--- App Examples/Console App/10.IncludeProject/Program.cs
+++ App Examples/Console App/10.IncludeProject/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using static System.Console;
+
+// Afegir referència a la llibreria BoscComa.Calculadora amb el següent comandament:
+// dotnet add reference ../../ClassLib/01.Calculator/
+
 using Calc = BoscComa.Calculadora.Calculadora;  // Using alias directive. El nom de l'espai de noms i el de la classe coincideixen i el compilador no sap diferenciar l'un de l'altre.
 
 namespace BoscComa.ConsoleApp

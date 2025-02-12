b2aabd40ad504427459962ef0c3df2f9561606cd
Miquel Boada <projecteinf@boscdelacoma.cat>
2025-02-12 20:22:53 +0100

    Programa per veure funcionament codi DTO sense BD

---
+++ App Examples/Console App/14.FuncionamentDTO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+public record CreateUserDto(string Name, string Email)
+{
+    public User ToModel() => new User(Guid.NewGuid().ToString(), Name, Email);
+}
+
+public class User
+{
+    public string Id { get; }
+    public string Name { get; }
+    public string Email { get; }
+
+    public User(string id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var dto = new CreateUserDto("Joan", "joan@email.com");
+        var user = dto.ToModel();
+
+        WriteLine($"DTO: {dto}");
+        WriteLine($"User: {user.Id}, {user.Name}, {user.Email}");
+    }
+}

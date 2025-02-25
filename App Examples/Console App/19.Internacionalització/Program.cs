20380f27aa6690393f5f3338057679902ac12c75
Miquel Boada <projecteinf@boscdelacoma.cat>
2025-02-25 16:53:31 +0100

    Internacionalització - Canvi de CurrentCulture

--- App Examples/Console App/19.Internacionalització/Program.cs
+++ App Examples/Console App/19.Internacionalització/Program.cs
@@ -6,8 +6,13 @@ public class Example0
 {
    public static void Main()
    {
-      // Display the name of the current culture.
-      WriteLine("CurrentCulture is {0}.", CultureInfo.CurrentCulture.Name);
-      WriteLine("CurrentUICulture is {0}", CultureInfo.CurrentUICulture.Name);
+        WriteLine("CurrentCulture is {0}.", CultureInfo.CurrentCulture.Name);
+        WriteLine("CurrentUICulture is {0}", CultureInfo.CurrentUICulture.Name);
+        WriteLine("En aquest moment a Catalunya: {0}", DateTime.Today);
+
+        var englishCulture = new CultureInfo("en-EN");
+        CultureInfo.CurrentCulture = englishCulture;
+        CultureInfo.CurrentUICulture = englishCulture;
+        WriteLine("En aquest moment a Anglaterra: {0}", DateTime.Today);
    }
 }

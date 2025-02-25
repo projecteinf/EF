11d321517c85516a3b9aa163122a86e44e61bb83
Miquel Boada <projecteinf@boscdelacoma.cat>
2025-02-25 16:45:32 +0100

    Internacionalització - Classe CultureInfo: CurrentCulture i CurrentUICulture. Aplicació exemple

--- App Examples/Console App/19.Internacionalització/Program.cs
+++ App Examples/Console App/19.Internacionalització/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+public class Example0
+{
+   public static void Main()
+   {
+      // Display the name of the current culture.
+      WriteLine("CurrentCulture is {0}.", CultureInfo.CurrentCulture.Name);
+      WriteLine("CurrentUICulture is {0}", CultureInfo.CurrentUICulture.Name);
+   }
+}

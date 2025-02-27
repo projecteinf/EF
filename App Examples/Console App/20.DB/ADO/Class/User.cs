4fa7165fd27c9a2edbbac0dfda601eeec36b201b
projecteinf <projecteinf@boscdelacoma.cat>
2025-02-27 04:27:05 -0700

    Crear propietats de la classe User com a Nullable

--- App Examples/Console App/20.DB/ADO/Class/User.cs
+++ App Examples/Console App/20.DB/ADO/Class/User.cs
@@ -9,7 +9,6 @@ namespace BoscComa.ADO
         public string? Name { get; set; }
         public string? Email { get; set; }   // Unic per a tots els usuaris
         public DateTime? DateOfBirth { get; set; }   // Guardem només la data. Visualització amb la cultura de l'usuari
-
         public void setPassword(string password) {
 
         }

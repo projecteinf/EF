28e26c0a3441f23bda85f9ea9c7a1c7ff2d54141
projecteinf <96139692+projecteinf@users.noreply.github.com>
2025-02-27 19:21:49 +0100

    Generar strings aleatoris

--- App Examples/Console App/20.DB/ADO/Helper/RandomGenerator.cs
+++ App Examples/Console App/20.DB/ADO/Helper/RandomGenerator.cs
@@ -2,6 +2,12 @@ namespace ADO.Helper
 {
     public class RandomGenerator
     {
-        
+        public static string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
     }
 }

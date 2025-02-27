58ac90b7773447269fa7e8789a8e268846ee64b1
projecteinf <projecteinf@boscdelacoma.cat>
2025-02-27 03:56:38 -0700

    Refrescat .gitignore per què no funcionava...

---
+++ App Examples/Console App/20.DB/ADO/Class/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BoscComa.ADO
+{
+    public class User
+    {
+        public string Uuid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }   // Unic per a tots els usuaris
+        public DateTime DateOfBirth { get; set; }   // Guardem només la data. Visualització amb la cultura de l'usuari
+    }
+}

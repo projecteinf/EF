c4741231b26815d102f774fd06ff209006eb65d1
projecteinf <projecteinf@boscdelacoma.cat>
2025-02-28 03:14:32 -0700

    Introducció de constant per la mida del hash password i passar constants a majúscules

--- App Examples/Console App/20.DB/ADO/Helper/Password.cs
+++ App Examples/Console App/20.DB/ADO/Helper/Password.cs
@@ -4,18 +4,19 @@ namespace BoscComa.ADO
 {
     static class Password
     {
-        private const int iterations = 100000;
-        private const int saltSize = 16;
+        private const int ITERATIONS = 100000;
+        private const int SALT_SIZE = 16;
+        private const int PASSWORD_HASH_SIZE = 16;
         
         public static byte[] GetHashPassword(string password, byte[] salt) {       
 
-            using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Password.iterations, HashAlgorithmName.SHA256))
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Password.ITERATIONS, HashAlgorithmName.SHA256))
             {
-                return pbkdf2.GetBytes(32); // 32 bytes
+                return pbkdf2.GetBytes(PASSWORD_HASH_SIZE); 
             }  
         }        
         public static byte[] GenerateSalt() {
-            byte[] salt = new byte[Password.saltSize];
+            byte[] salt = new byte[Password.SALT_SIZE];
             using (var rng = RandomNumberGenerator.Create())
             {
                 rng.GetBytes(salt);

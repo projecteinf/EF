2c4d2c268afaa1400efc6c5db1ed1c4f6a375e16
projecteinf <projecteinf@boscdelacoma.cat>
2025-03-03 03:43:32 -0700

    Creació classe personalitzada per a tractar excepcions en fitxers

---
+++ App Examples/Console App/20.DB/XifratgeStringConnection/Model/Classes/Exception/FileException.cs
@@ -0,0 +1,10 @@
+using System.IOException;
+namespace BoscComa.GestioErrors
+{
+    public class FileException : IOException
+    {
+        public FileException() : base() { }
+        public FileException(string message) : base(message) { }
+        public FileException(string message, Exception innerException): base(message, innerException) { }
+    }
+}

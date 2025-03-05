e11ea59a64a52772dfd0ab623202396d27e951ad
projecteinf <projecteinf@boscdelacoma.cat>
2025-03-05 04:35:29 -0700

    Programa principal - comprovació emmagatemar cadena connexió xifrada

--- App Examples/Console App/20.DB/XifratgeStringConnection/Program.cs
+++ App Examples/Console App/20.DB/XifratgeStringConnection/Program.cs
@@ -1,5 +1,5 @@
 ﻿using static System.Console;
-using BoscComa.Xifratge;
+using BoscComa.GestioErrors;
 
 namespace BoscComa.Connexio 
 {
@@ -7,9 +7,20 @@ namespace BoscComa.Connexio
     {
         static void Main()
         {
-            DadesXifratgeAES dadesXifratge = DadesXifratgeAES.XifratgeAES;
-            WriteLine("Key: {0}",Convert.ToBase64String(dadesXifratge.ObtenirClau()));
-            WriteLine("Vector inicialtizació: {0}",Convert.ToBase64String(dadesXifratge.ObtenirVectorInicialitzacio()));
+            // DadesXifratgeAES dadesXifratge = DadesXifratgeAES.XifratgeAES;
+            // WriteLine("Key: {0}",Convert.ToBase64String(dadesXifratge.ObtenirClau()));
+            // WriteLine("Vector inicialtizació: {0}",Convert.ToBase64String(dadesXifratge.ObtenirVectorInicialitzacio()));
+
+            StringConnection stringConnection = new StringConnection("localhost", "dbDemo", "SA", "Patata1234" );
+            try
+            {
+                stringConnection.Store(@"/home/projecteinf/", "connexio.dat", overwrite: true);
+            }
+            catch (FileException ex)
+            {
+                Console.WriteLine($"Error: {ex.TipusError} - {ex.Message}");
+            }
+
         }
     }
 }

0338df1403ad040d5c1a4613e8c124ecf4ce11ec
projecteinf <projecteinf@boscdelacoma.cat>
2025-03-06 04:35:40 -0700

    Canvi a programa principal per a fer test de connexió a SGBD

--- App Examples/Console App/20.DB/ADO/Program.cs
+++ App Examples/Console App/20.DB/ADO/Program.cs
@@ -11,37 +11,7 @@ namespace BoscComa.AppERP
         
         public static void Main() 
         {
-            List <string> passwordClear = new List<string>();
-            List <User> users = new List<User>();
-            for(short i=0;i<10;i++) 
-            {
-                users.Add(Program.CreateRandomUser(passwordClear));
-            }
-            short index = 0;
-            foreach (User user in users)
-            {
-                WriteLine(user.ToString());
-                WriteLine($"Password clear: {passwordClear[index++]}");
-            }
             
-            Program.VerificarPassword(users[0],passwordClear[0]);
-            Program.VerificarPassword(users[0],passwordClear[1]);
-        }
-
-        public static User CreateRandomUser(List<string> passwordClear) 
-        {
-            User user = new User();
-            user.Name = RandomGenerator.GenerateRandomString(6);
-            string password = RandomGenerator.GenerateRandomString(8);
-            user.SetPassword(password);
-            passwordClear.Add(password);
-            return user;
-        }
-
-        public static void VerificarPassword(User user,string password)
-        {
-            string result = user.VerifyPassword(password) ? "Password correcte" : "Password incorrecte";
-            WriteLine(result);
         }
     }
 }

407d824f010459c6958cde7bbdc6708cbbe2055c
projecteinf <projecteinf@boscdelacoma.cat>
2025-03-06 04:44:51 -0700

    Canviar nom variable local filename per fileName

--- App Examples/Console App/20.DB/XifratgeStringConnection/Program.cs
+++ App Examples/Console App/20.DB/XifratgeStringConnection/Program.cs
@@ -8,18 +8,18 @@ namespace BoscComa.Connexio
         static void Main()
         {
             string path=@"/home/projecteinf/Projectes/2025/EF/App Examples/Console App/20.DB/ADO/Config";
-            string filename=@"connction.enc";
+            string fileName=@"connction.enc";
 
             StringConnection stringConnection = new StringConnection("localhost", "dbDemo", "SA", "Patata1234" );
             try
             {
-                stringConnection.Store(path, filename, overwrite: true);
+                stringConnection.Store(path, fileName, overwrite: true);
             }
             catch (FileException ex)
             {
                 Console.WriteLine($"Error: {ex.TipusError} - {ex.Message}");
             }
-            WriteLine(StringConnection.GetDecrypt(path,filename));
+            WriteLine(StringConnection.GetDecrypt(path,fileName));
         }
     }
 }

8a05a39f73f26e578ac1b29d75137f38ba113422
projecteinf <projecteinf@boscdelacoma.cat>
2025-03-18 14:14:49 -0600

    Creació context per nova versió EF

--- App Examples/Console App/20.DB/EF/DBContext/AppDbContext.cs
+++ App Examples/Console App/20.DB/EF/DBContext/AppDbContext.cs
@@ -7,14 +7,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BoscComa.EF
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-        public DbSet<Item> Items { get;set; }
-    }
-
-    protected override void OnConfiguring(DBContextOptionsBuilder options)
-    {
+        public DbSet<Item> Items { get; set; }
+    
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
         
     }
 }
--- App Examples/Console App/20.DB/EF/Program.cs
+++ App Examples/Console App/20.DB/EF/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// <summary>
+///     dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+///     dotnet add package Microsoft.EntityFrameworkCore.Design
+/// </summary>
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BoscComa.Connexio;
+namespace BoscComa.EF 
+{
+    public class Program {
+        static void Main()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(ConnectToDB()))
+                .BuildServiceProvider();
+
+                using var context = serviceProvider.GetRequiredService<AppDbContext>();
+                context.Database.Migrate();
+        }
+
+        private static string ConnectToDB() 
+        {
+            string path=@"/home/projecteinf/Projectes/2025/EF/App Examples/Console App/20.DB/EF/Config";
+            string fileName=@"connction.enc";
+            return StringConnection.GetDecrypt(path,fileName);
+        }
+    }
+}

99c8d9066a6a2dc96c13e0097ec775892b82f6c3
projecteinf <projecteinf@boscdelacoma.cat>
2025-03-25 04:43:08 -0600

    Generació d'access token amb millores

--- App Examples/Console App/20.DB/ADOExample/Helper/Token.cs
+++ App Examples/Console App/20.DB/ADOExample/Helper/Token.cs
@@ -15,16 +15,33 @@ namespace BoscComa.ADO
         static public string GenerateJwtToken(User user)
         {
             DadesXifratgeAES xifratge = DadesXifratgeAES.XifratgeAES;
+            byte[] keyBytes = xifratge.GetKey();
+
+            if (keyBytes == null || keyBytes.Length < 32) 
+                throw new InvalidOperationException("La clau de xifratge no és vàlida.");
+            
             SymmetricSecurityKey symmetricSecurityKey = new SymmetricSecurityKey(xifratge.GetKey());
             SigningCredentials signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
 
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Uuid.ToString()),   
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),         
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // Unique identifier del token
+                new Claim("role", "Usuari") 
+            };
+
             JwtSecurityToken token = new JwtSecurityToken(
-                claims: new List<Claim>(),
-                expires: DateTime.Now.AddMinutes(30),
+                issuer: "Bosc de la Coma",
+                audience: "Bosc de la Coma",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(1),
                 signingCredentials: signingCredentials
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
+
+
     }
 }

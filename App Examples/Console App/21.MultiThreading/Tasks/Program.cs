7296ebebccffebed53a909db145af5e5d37921cd
projecteinf <projecteinf@boscdelacoma.cat>
2025-04-01 08:53:13 -0600

    Multithreading - execució síncrona. No paral·lelisme!

---
+++ App Examples/Console App/21.MultiThreading/Tasks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+
+class Program {
+    static void Main() {
+        var timer = Stopwatch.StartNew();
+        WriteLine("Execució síncrona");
+        MethodA();
+        MethodB();
+        MethodC();
+        WriteLine($"Temps síncron: {timer.ElapsedMilliseconds} ms");
+    }
+    static void MethodA()
+    {
+        WriteLine("Iniciat mètode A");
+        Thread.Sleep(3000);
+        WriteLine("Finalitzat mètode A");
+    }
+    static void MethodB()
+    {
+        WriteLine("Iniciat mètode B");
+        Thread.Sleep(2000);
+        WriteLine("Finalitzat mètode B");
+    }
+    static void MethodC()
+    {
+        WriteLine("Iniciat mètode C");
+        Thread.Sleep(1000);
+        WriteLine("Finalitzat mètode C");
+    }
+}

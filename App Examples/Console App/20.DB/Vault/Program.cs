dcc2939556bce4e39817fcc5b8e1273fb84573dd
projecteinf <projecteinf@boscdelacoma.cat>
2025-04-09 12:40:44 -0600

    Connexió comprovant certificat digital. Protecció contra attack 'Man In The Middle'

--- App Examples/Console App/20.DB/Vault/Program.cs
+++ App Examples/Console App/20.DB/Vault/Program.cs
@@ -2,12 +2,25 @@
     Instal·lació paquets
         dotnet add package Newtonsoft.json 2017  
         dotnet add package Microsoft.Data.SqlClient
+
+    Per a obtenir l'emprenta del certificat
+
+    openssl x509 -in /home/projecteinf/Projectes/2025/EF/Docs/Docker/Vault/Produccio/certs/vault.crt -noout -fingerprint -sha1
+
+    ThumbPrint -> sha1 Fingerprint=04:E6:38:0C:D5:F9:62:E7:BC:39:94:45:64:E3:0F:02:92:70:80:90
+
+    Cal eliminar els ":"
+
+    04E6380CD5F962E7BC39944564E30F0292708090
+
 */ 
 
 
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json.Linq;
 using Microsoft.Data.SqlClient;
 
@@ -21,7 +34,17 @@ class Program
 
         HttpClientHandler handler = new HttpClientHandler
         {
-            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            
+            ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            {
+                const string expectedThumbprint = "04E6380CD5F962E7BC39944564E30F0292708090";
+                if (sslPolicyErrors == SslPolicyErrors.None)
+                {
+                    return true;
+                }
+                string actualThumbprint = cert?.GetCertHashString()?.ToUpperInvariant();
+                return actualThumbprint == expectedThumbprint;
+            }
         };
 
         using (HttpClient client = new HttpClient(handler))

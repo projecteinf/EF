71a80849b474497b5c4cedb62eaadca2be263338
projecteinf <projecteinf@boscdelacoma.cat>
2025-04-09 12:06:01 -0600

    Connexió a base de dades utilitzant Vault. Sense utilitzar classesgit add .

--- App Examples/Console App/20.DB/Vault/Program.cs
+++ App Examples/Console App/20.DB/Vault/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static async System.Threading.Tasks.Task Main(string[] args)
+    {
+        string vaultToken = "hvs.qj8dN3I23ylUkPJHncl4Wpu6"; // El teu token root
+        string vaultAddress = "https://127.0.0.1:8200";     // L'adreça del teu Vault
+        string secretPath = "secrets/config/mssql";       // Ruta on Vault exposa les credencials
+
+        HttpClientHandler handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        using (HttpClient client = new HttpClient(handler))
+        {
+            client.BaseAddress = new Uri(vaultAddress);
+            client.DefaultRequestHeaders.Add("X-Vault-Token", vaultToken);
+
+            HttpResponseMessage response = await client.GetAsync($"/v1/{secretPath}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                JObject data = JObject.Parse(json)["data"] as JObject;
+
+                string username = data["username"].ToString();
+                string password = data["password"].ToString();
+
+                Console.WriteLine($"Usuari: {username}");
+                Console.WriteLine($"Password: {password}");
+
+                // Connexió a la base de dades
+                string connectionString = $"Server=localhost;Database=dbDemo;User Id={username};Password={password};TrustServerCertificate=True;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connexió amb èxit!");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error accedint a Vault: {response.StatusCode}");
+            }
+        }
+    }
+}
+
